3b062dcb68ebd6b09b25bb6f2c3adc7abd7e74ef
Bas Stoker <bas@orangebeard.io>
2022-11-13 12:10:16 +0100

    Initial source from start.spring.io

---
+++ src/test/java/nl/jnext/workshop/testcontainers/vakantieplanner/VakantieplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.jnext.workshop.testcontainers.vakantieplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VakantieplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
